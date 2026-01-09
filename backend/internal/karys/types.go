4c40aaa069ea4e9fd06928bd843fd193a8631c8c
tkozakas <tomas.kozakas@gmail.com>
2026-01-09 13:05:40 +0200

    Include info field in API response

--- backend/internal/karys/types.go
+++ backend/internal/karys/types.go
@@ -7,7 +7,7 @@ type Person struct {
 	Lastname   string `json:"lastname"`
 	Bdate      string `json:"bdate"`
 	Department string `json:"-"`
-	Info       string `json:"-"`
+	Info       string `json:"info"`
 }
 
 type Region struct {
